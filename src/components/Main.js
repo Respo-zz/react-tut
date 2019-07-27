b3f7b7a4f81cd94c28dfa3fef9a414a3bda84805
nofalta <renato@asimov.cl>
2019-07-27 13:34:27 -0400

    Componente y Estilos Lista

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './style/Main.css';
 
+import TodoList from "./TodoList";
+
 function Main() {
   return (
     <div>
       <h1> Contenido Main </h1>
+      <div className="todo-list">
+        <TodoList />
+        <TodoList />
+        <TodoList />
+        <TodoList />
+        <TodoList />
+      </div>
     </div>
   );
 }
---
+++ src/components/TodoList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style/TodoList.css';
+
+function TodoList() {
+  return (
+    <div className="todo-item">
+      <input type="checkbox" />
+      <p> Placeholder Text</p>
+    </div>
+  );
+}
+
+export default TodoList;
